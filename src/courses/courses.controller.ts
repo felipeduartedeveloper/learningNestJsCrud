744dd8f0754862b1d2b3202dadb8901c4adc0ac8
felipeduartedelevoper <felipesilvapac@gmail.com>
2022-01-10 17:41:37 -0300

    request data methods

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,12 +3,9 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -17,7 +14,7 @@ export class CoursesController {
   constructor(private readonly courseService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
+  findAll() {
     return this.courseService.findAll();
   }
 
