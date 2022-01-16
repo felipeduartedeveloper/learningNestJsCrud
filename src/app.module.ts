a0b296221a80517b1ade145b50513ca6f2b18ae8
felipeduartedeveloper <felipesilvapac@gmail.com>
2022-01-15 23:54:06 -0300

    config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,19 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
